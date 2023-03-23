d17a25924837ed86ef77d1864d2b63926b0a9f49
pwz2k <pwz2k@hotmail.com>
2023-03-22 20:40:12 -0400

    Delete labelaxxess_backend directory

--- labelaxxess_backend/routes/TestRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const router = require("express").Router();
-const Controller = require("../controllers/TestController");
-
-// Authentication
-router.get("/generateBarcode", Controller.gs1Barcode);
-
-module.exports = router;
--- labelaxxess_backend/routes/WebhookRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const router = require("express").Router();
-const Controller = require("../controllers/WebhookController");
-
-// Authentication
-router.post("/coinbase", Controller.coinbase);
-
-module.exports = router;
